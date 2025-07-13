c66f15da4b4ef5ca034475dd08a85dd0487487f5
freemanofblackmesa <neartad@gmail.com>
2025-07-13 14:25:24 +0300

    Initial JavaScripe Push

---
+++ Courses/Bro Code/1 - Initial/script.js
@@ -0,0 +1,14 @@
+console.log("I like pizza");
+console.log("It's really good!");
+
+// to create an alert box use "window.alert("")"
+window.alert("I REALLY LOVE PIZZA!")
+
+// This is a comment
+/*
+    This
+    is
+    a
+    multiline
+    comment
+*/
