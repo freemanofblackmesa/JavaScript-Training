e8830950b9c4f3eae09b7a7e517dfd06af40cb92
freemanofblackmesa <neartad@gmail.com>
2025-07-13 15:35:26 +0300

    varianles final

---
+++ Courses/Bro Code/2 - Variables and Data Types in JavaScript/script.js
@@ -0,0 +1,7 @@
+/* A variable is a container for storing data
+A variable behves as if it was the value that is contains
+
+Two steps:
+1. Declaration (var, let, const)
+2. Assignment (= assignment operator)
+*/
