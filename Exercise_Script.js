cadc2870b3e0d2b81c7b9946a841d219d701bacc
freemanofblackmesa <neartad@gmail.com>
2025-09-09 16:54:20 +0300

    Modification

--- Exercise_Script.js
+++ Exercise_Script.js
@@ -1,28 +1,18 @@
-const Roll = document.getElementById('roll');
-const Reset = document.getElementById('reset');
-const Label1 = document.getElementById('label1');
-const Label2 = document.getElementById('label2');
-const Label3 = document.getElementById('label3');
-let randomNum1;
-let randomNum2;
-let randomNum3;
-let min = 1;
-let max = 6;
-let reset = 0;
+const myLabel = document.getElementById('mylabel');
+const decreaseBtn = document.getElementById('decreasebtn');
+const resetBtn = document.getElementById('resetbtn');
+const increaseBtn = document.getElementById('increasebtn');
+let count = 0;
 
-Roll.onclick = function(){
-    randomNum1 = Math.floor(Math.random() * max) + min;
-    Label1.innerHTML = randomNum1;
-    randomNum2 = Math.floor(Math.random() * max) + min;
-    Label2.innerHTML = randomNum2;
-    randomNum3 = Math.floor(Math.random() * max) + min;
-    Label3.innerHTML = randomNum3;
+decreaseBtn.onclick = function(){
+    count--;
+    myLabel.innerHTML = count;
 }
-Reset.onclick = function(){
-    randomNum1 = reset;
-    Label1.innerHTML = randomNum1;
-    randomNum2 = reset;
-    Label2.innerHTML = randomNum2;
-    randomNum3 = reset;
-    Label3.innerHTML = randomNum3;
+resetBtn.onclick = function(){
+    count = 0;
+    myLabel.innerHTML = count;
+}
+increaseBtn.onclick = function(){
+    count++;
+    myLabel.innerHTML = count;
 }
